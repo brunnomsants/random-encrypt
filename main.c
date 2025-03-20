1f270d4482abb4ea9b80bdf384de880fb58ed509
brunnomsants <brunnomurilo1@gmail.com>
2025-03-20 14:31:18 -0300

    Base64 update

--- main.c
+++ main.c
@@ -36,17 +36,20 @@ const char* binary(int num) {
 }
 
 void base64(char key[]) {
-    char prebinary[1000], binarys[100][7], base64_char[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";  
+    char prebinary[1000], binarys[100][7], base64_char[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";  
     prebinary[0] = '\0';
     int cont = 0, j = 0, binaryascii[100] = {0};  
     size_t length = strlen(key);
 
+    // Convert the input string to binary
     for (int i = 0; i < length; i++) {
         strcat(prebinary, binary(ascii(key[i])));
     }
-    
+    strcat(prebinary, "00001010");  // Add newline character as binary (0x0A)
+
     int preblen = strlen(prebinary);
     
+    // Break the binary string into 6-bit chunks
     for (int i = 0; i < preblen; i++) {
         binarys[cont][j] = prebinary[i]; 
         j++;
@@ -57,37 +60,53 @@ void base64(char key[]) {
             j = 0;   
         }
     }
-    
+
+    // Padding the last binary chunk if needed
     for (int i = 0; i < strlen(binarys[cont]); i++) {
         if(binarys[cont][i] != '0' && binarys[cont][i] != '1') {
             binarys[cont][i] = '0';
         }
     }
 
+    // Reverse the bits in each chunk and ensure it has 6 bits
     for (int i = 0; i <= cont; i++) {
         binarys[i][6] = '\0';
-    }
-
-    for (int i = 0; i <= cont; i++) {
         reversestring(binarys[i]);
     }
 
-    // Convert binary to base64
-    for (int i = 0; i <= cont; i++) {
+    // Convert each 6-bit binary chunk to decimal and then to base64
+    int cont24 = 0;
+    for (int i = 0; i < cont; i++) {
         binaryascii[i] = 0; 
         for(int j = 5; j >= 0; j--) {
             if(binarys[i][j] == '1'){
                 binaryascii[i] += pow(2, j);
             }
         }
-        printf("================\n%s\n%d\n%c\n", binarys[i], binaryascii[i], base64_char[binaryascii[i]]);
+        printf("\n%s\n", binarys[i]);
+        cont24++;
+        printf("%c", base64_char[binaryascii[i]]);
     }
 
-    printf("================\n");
+    printf("\n\n\n");
+    for(int i =0; i<cont; i++){
+        printf("%c", base64_char[binaryascii[i]]);
+    }
+    if(cont24 > 0) {
+        int remainder = cont24 % 4;
+        if (remainder == 2) {
+            printf("==");
+        } else if (remainder == 1) {
+            printf("=");
+        }
+    }
+    printf("\n\n\n");
+    
 }
 
 int main() {
     scanf("%[^\n]", key);  
+    printf("\n");
     base64(key);  
     return 0;
 }
